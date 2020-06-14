c6c8242150ae9094febec28eaee02ddf7fb7508f
Jens Petersen <petersen@redhat.com>
2020-06-14 23:56:25 +0800

    add snapshots commands and refactor

--- Main.hs
+++ Main.hs
@@ -1,6 +1,6 @@
 import Control.Monad.Extra
 import Data.List.Extra
-import SimpleCmd
+import SimpleCmd hiding (ifM)
 import SimpleCmdArgs
 import System.Directory
 import System.FilePath
@@ -18,41 +18,50 @@ main = do
       cleanStackWork <$> optional (strArg "PROJECTDIR")
     ]
 
--- number of builds to keep per ghc version
-keep_dirs :: Int
-keep_dirs = 4
-
-cleanSnapshots :: IO ()
-cleanSnapshots = do
-  error' "write-me"
-
 cleanStackWork :: Maybe FilePath -> IO ()
 cleanStackWork mdir = do
   whenJust mdir $ \ dir -> setCurrentDirectory dir
-  unlessM (doesDirectoryExist ".stack-work") $
-    error' "No .stack-work/ found"
-  setCurrentDirectory ".stack-work/install/"
+  switchToSystemDirUnder ".stack-work/install"
+  -- number of builds to keep per ghc version
+  cleanAwayOldBuilds 4
+
+cleanSnapshots :: IO ()
+cleanSnapshots = do
+  home <- getHomeDirectory
+  switchToSystemDirUnder $ home </> ".stack/snapshots"
+  -- number of snapshots to keep per ghc version
+  cleanAwayOldBuilds 50
+
+switchToSystemDirUnder :: FilePath -> IO ()
+switchToSystemDirUnder dir = do
+  ifM (doesDirectoryExist dir)
+    (setCurrentDirectory dir)
+    (error' $ dir ++ "not found")
   systems <- listDirectory "."
   let system = case systems of
-        [] -> error' "Did not find any OS systems in .stack-work/install/"
+        [] -> error' $ "No OS system in " ++ dir
         [sys] -> sys
-        _ -> error' "More than one OS systems found (unsupported)"
+        _ -> error' "More than one OS systems found " ++ dir ++ " (unsupported)"
   setCurrentDirectory system
+
+cleanAwayOldBuilds :: Int -> IO ()
+cleanAwayOldBuilds keep = do
   -- sort and then group by ghc version
   dirs <- sortOn takeFileName . lines <$> shell ( unwords $ "ls" : ["-d", "*/*"])
   let ghcs = groupOn takeFileName dirs
   mapM_ removeOlder ghcs
   where
+    removeOlder :: [FilePath] -> IO ()
     removeOlder dirs = do
-      oldfiles <- drop keep_dirs . reverse <$> sortByAge dirs
+      oldfiles <- drop keep . reverse <$> sortedByAge
       mapM_ (removeDirectoryRecursive . takeDirectory) oldfiles
+      where
+        sortedByAge = do
+          fileTimes <- mapM newestTimeStamp dirs
+          return $ map fst $ sortOn snd fileTimes
 
-    sortByAge dirs = do
-      fileTimes <- mapM newestTimeStamp dirs
-      return $ map fst $ sortOn snd fileTimes
-
-    newestTimeStamp dir = do
-      withCurrentDirectory dir $ do
-        files <- listDirectory "."
-        timestamp <- last . sort <$> mapM getModificationTime files
-        return (dir, timestamp)
+        newestTimeStamp dir = do
+          withCurrentDirectory dir $ do
+            files <- listDirectory "."
+            timestamp <- maximum <$> mapM getModificationTime files
+            return (dir, timestamp)
