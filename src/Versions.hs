f3ef43704a241cfcc1ad651c8579d8e1faf84d95
Jens Petersen <juhpetersen@gmail.com>
2025-12-29 19:31:37 +0800

    Versions makeVersion: avoid init

--- src/Versions.hs
+++ src/Versions.hs
@@ -9,11 +9,10 @@ import SimpleCmd ((+-+))
 
 majorVersion :: Version -> Version
 majorVersion ver =
-  let vernums = versionBranch ver in
-    case length vernums of
-      2 -> ver
-      3 -> (makeVersion . init) vernums
-      _ -> error $ "Bad ghc version" +-+ showVersion ver
+  case versionBranch ver of
+    [_,_] -> ver
+    (v1:v2:_) -> makeVersion [v1,v2]
+    _ -> error $ "Bad ghc version" +-+ showVersion ver
 
 isMajorVersion :: Version -> Bool
 isMajorVersion ver =
